7a402568aacb4acd09825055c6ebaef8ed06f112
Kyle Durand <kyle.durand@shopify.com>
2016-06-28 23:25:29 -0400

    Adding syntax to contest page

--- index.php
+++ index.php
@@ -6,21 +6,20 @@
   $keywords = "Small engine repair, lawn care, serving South Florida for over 30 years, lawn equipment maintenance";
 ?>
 
-<?php include 'components/header.php'; ?>
+<?php
+  include 'components/header.php';
+  include 'components/slider.php';
+?>
 
-<?php include 'components/slider.php'; ?>
-
-<h2 class="type--heading type--thin type--slate type--underlined type--centered">Directions</h2>
-
-<!--
-<section class="iframe-wrapper">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14341.213914202695!2d-80.2431056!3d26.0236246!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6fc8c4f039d67c4!2sBitter+Blue+Lawn+%26+Garden!5e0!3m2!1sen!2sus!4v1465780404788" allowfullscreen scrolling="no" seamless="seamless"></iframe>
-</section>
--->
+<h2 class="type--heading type--thin type--slate type--underlined type--centered">
+  Directions
+</h2>
 
 <?php include 'components/map.php'; ?>
 
-<h2 class="type--heading type--thin type--slate type--underlined type--centered">About us</h2>
+<h2 class="type--heading type--thin type--slate type--underlined type--centered">
+  About us
+</h2>
 
 <p class="text-block">
   This would be a large text block. This would be a large text block. This
@@ -30,7 +29,9 @@
   would be a large text block.
 </p>
 
-<h2 class="type--heading type--thin type--slate type--underlined type--centered">Official dealers of</h2>
+<h2 class="type--heading type--thin type--slate type--underlined type--centered">
+  Official dealers of
+</h2>
 
 <div class="dealer-logos">
   <img class="dealer-logo" src="../images/logo-stihl.png" alt="Stihl">
